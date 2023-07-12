2e580fe825ab00fcf57f4501f80ccd4fa636c59e
Leaaaf <alessandro.foglia@outlook.it>
2023-07-12 22:20:14 +0200

    fix

--- frontend/src/components/FileCard/FileCardContainer.tsx
+++ frontend/src/components/FileCard/FileCardContainer.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function FileCardContainer({...props}: Props) {
     return (
-        <div className="mt-12 flex flex-row felx-wrap w-full gap-6">
+        <div className="mt-12 flex flex-row flex-wrap w-full gap-6">
             {props.files.map(el => (
                 <FileCard key={el.id} file={el}></FileCard>
             ))}
