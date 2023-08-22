58fb9672720157a57bbe8901d17850c6879305a5
Leaaaf <alessandro.foglia@outlook.it>
2023-08-22 17:43:28 +0200

    generate files from template

--- backend/src/blockchain/blockchain.service.ts
+++ backend/src/blockchain/blockchain.service.ts
@@ -1,10 +1,8 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { User } from 'src/users/entities/user';
-import * as FabricCAServices from 'fabric-ca-client';
-import { Wallets, X509Identity } from 'fabric-network';
+import { readFile, readdir, writeFile } from 'fs/promises';
 import * as fs from 'fs';
 import * as path from 'path';
-import { readFile, writeFile } from 'fs/promises';
 
 /**
  * This service provides methods to invoke scripts in order to interact with network blockchain
@@ -14,8 +12,8 @@ export class BlockchainService {
     private readonly logger = new Logger(BlockchainService.name);
 
     private readonly BASE_PATH = path.join(process.cwd(), 'src', 'blockchain');
-    private readonly SCRIPT_DIR_PATH = path.join(this.BASE_PATH, 'script');
-    private readonly CRYPTO_DIR_PATH = path.join(this.BASE_PATH, 'organizations', 'cryptogen');
+    private readonly SCRIPT_DIR_PATH = path.join(this.BASE_PATH, 'scripts', 'org-template-scripts');
+    private readonly ORG_DIR_PATH = path.join(this.BASE_PATH, 'addOrgTemplate');
 
     constructor() {}
 
@@ -30,11 +28,14 @@ export class BlockchainService {
         // 1. Generate files from templates
         await this.generateOrgFiles(user);
 
-        // 2. Invoke generated scripts
+        // 2. Invoke generated scripts [ joining channel , create private data collection with main organization]
 
         // 3. Create org wallet
 
         this.logger.log('Organization ' + user.organization + ' enrolled');
+
+        // For testing purposes
+        throw new HttpException('Error during Org initialization', HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     /**
@@ -43,19 +44,24 @@ export class BlockchainService {
      * @returns 
      */
     private async generateOrgFiles(user: User) {
-        const FILES = ['createOrgTemplate.sh', 'registerEnrollTemplate.sh', 'crypto-config-org-template.yaml'];
-
         this.logger.log('Start generating files for organization: ' + user.organization);
-
-        const f1 = await this.generateFromTemplate(user, this.SCRIPT_DIR_PATH, FILES.at(0));
-        const f2 = await this.generateFromTemplate(user, this.SCRIPT_DIR_PATH, FILES.at(1));
-        const f3 = await this.generateFromTemplate(user, this.CRYPTO_DIR_PATH, FILES.at(2));
-
-        if (f1 && f2 && f3) {
-            this.logger.log('Files generated for organization: ' + user.organization);
-            return [f1, f2, f3];
-        }
         
+        // Generating folder structure in order to save new files
+        await this.generateDirStructure(user);
+
+        // Read all template files
+        let generatedFiles: Array<String> = new Array<String>();
+        const orgTemplateFiles: string[] = (await this.getFileList(this.ORG_DIR_PATH)).concat(...(await this.getFileList(this.SCRIPT_DIR_PATH)));
+
+        // Convert each template to a new file needed to create the new organization
+        for(const file of orgTemplateFiles) {
+            generatedFiles.push(await this.generateFromTemplate(user, file));
+        }
+
+        if(generatedFiles.length == orgTemplateFiles.length)
+            return generatedFiles;
+
+        this.logger.warn(`The number generated files (${generatedFiles.length}) is not corresponding to (${orgTemplateFiles.length})`)
         throw new HttpException('Error during Org initialization', HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
@@ -66,17 +72,18 @@ export class BlockchainService {
      * @param fileName file name
      * @returns 
      */
-    private async generateFromTemplate(user: User, dirPath: string, fileName: string): Promise<String> {
+    private async generateFromTemplate(user: User, fileName: string): Promise<String> {
         const ORGANIZATION_NAME_PLACEHOLDER = 'ORGANIZATION_NAME_PLACEHOLDER';
         const ORGANIZATION_DOMAIN_PLACEHOLDER = 'ORGANIZATION_DOMAIN_PLACEHOLDER';
-        
-        const newFilePath = path.join(dirPath, 'generated', fileName
+
+        const newFilePath = path.join(fileName.replace('addOrgTemplate', 'generated/addOrg' + user.name + user.surname)
+            .replace('org-template-scripts', 'generated/org-' + user.name.toLowerCase() + user.surname.toLowerCase() + '-scripts')
             .replace('Template', user.name + user.surname)
             .replace('template', user.name.toLowerCase() + user.surname.toLowerCase()));
 
         try {
             this.logger.log('Generating file: ' + newFilePath);
-            let data: string = (await readFile(path.join(dirPath, fileName))).toString();
+            let data: string = (await readFile(fileName)).toString();
 
             data = data.replaceAll(ORGANIZATION_NAME_PLACEHOLDER, user.name + user.surname);
             data = data.replaceAll(ORGANIZATION_DOMAIN_PLACEHOLDER, user.organization);
@@ -89,4 +96,44 @@ export class BlockchainService {
             return undefined;
         }
     }
+
+    private async generateDirStructure(user: User) {
+        const dirPath = [
+            path.join('generated', 'addOrg' + user.name + user.surname, 'compose', 'docker', 'peercfg'),
+            path.join('generated', 'addOrg' + user.name + user.surname, 'compose', 'podman', 'peercfg'),
+            path.join('generated', 'addOrg' + user.name + user.surname, 'fabric-ca', 'org-' + user.name.toLowerCase() + user.surname.toLowerCase()),
+            path.join('scripts', 'generated', 'org-' + user.name.toLowerCase() + user.surname.toLowerCase() + '-scripts'),
+        ]
+
+        let currDirPath: string;
+
+        dirPath.forEach(folderStruct => {
+            currDirPath = this.BASE_PATH;
+            folderStruct.split(path.sep).forEach(el => {
+                currDirPath = path.join(currDirPath, el);
+                // If folder not exists generate it
+                if(!fs.existsSync(currDirPath))
+                    fs.mkdirSync(currDirPath);
+            })
+        })
+            
+    }
+
+    async getFileList(dirName: string): Promise<string[]> {
+        let files: string[] = [];
+        const items = await readdir(dirName, { withFileTypes: true });
+    
+        for (const item of items) {
+            if (item.isDirectory()) {
+                files = [
+                    ...files,
+                    ...(await this.getFileList(`${dirName}/${item.name}`)),
+                ];
+            } else {
+                files.push(`${dirName}/${item.name}`);
+            }
+        }
+    
+        return files;
+    };
 }
