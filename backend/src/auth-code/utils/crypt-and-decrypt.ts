2b81898a5f762db7a2486c274caed90472ebf66d
Leaaaf <alessandro.foglia@outlook.it>
2024-01-18 18:12:37 +0100

    bcrypt enabled

--- backend/src/auth-code/utils/crypt-and-decrypt.ts
+++ backend/src/auth-code/utils/crypt-and-decrypt.ts
@@ -1,11 +1,9 @@
-// import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 export async function hash(password: string): Promise<string> {
-  // return await bcrypt.hash(password, await bcrypt.genSalt());
-  return password;
+  return await bcrypt.hash(password, await bcrypt.genSalt());
 }
 
 export async function isMatch(password, hash): Promise<boolean> {
-  // return await bcrypt.compare(password, hash);
-  return password === hash;
+  return await bcrypt.compare(password, hash);
 }
