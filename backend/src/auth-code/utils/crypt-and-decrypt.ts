ce0a5546d570f5404483b076c430eb3a80d48144
Leaaaf <alessandro.foglia@outlook.it>
2024-01-18 11:43:02 +0100

    removed bcrypt lib

--- backend/src/auth-code/utils/crypt-and-decrypt.ts
+++ backend/src/auth-code/utils/crypt-and-decrypt.ts
@@ -1,9 +1,11 @@
-import * as bcrypt from 'bcrypt';
+// import * as bcrypt from 'bcrypt';
 
 export async function hash(password: string): Promise<string> {
-  return await bcrypt.hash(password, await bcrypt.genSalt());
+  // return await bcrypt.hash(password, await bcrypt.genSalt());
+  return password;
 }
 
 export async function isMatch(password, hash): Promise<boolean> {
-  return await bcrypt.compare(password, hash);
+  // return await bcrypt.compare(password, hash);
+  return password === hash;
 }
